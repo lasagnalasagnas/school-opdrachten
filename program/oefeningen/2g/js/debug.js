74a8aaeaac6e27d1fb0d23f80177eeeceee422ff
Turgay Simeonov <102825@glr.nl>
2025-01-16 11:00:56 +0000

    oefening 2g

--- program/oefeningen/2g/js/debug.js
+++ program/oefeningen/2g/js/debug.js
@@ -1,7 +1,10 @@
+// De variabele 'a' wordt gebruikt voordat deze is gedeclareerd, wat een ReferenceError veroorzaakt.
+// Om dit op te lossen, declareer de variabele 'a' voordat je deze gebruikt.
+
+let a = 1;
 console.log(a);
 console.log(foo());
 
-let a = 1;
 function foo(){
     return 2;
 }
